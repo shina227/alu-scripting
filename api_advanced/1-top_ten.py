68bcc84a45e929d059216decf0f2393dbee3d829
Shina <m.atete@alustudent.com>
2025-02-21 15:11:37 +0200

    Changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,21 +4,20 @@ import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
+        """Return number of subscribers if @subreddit is valid subreddit.
+            if not return 0."""
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+                headers = {'User-Agent': 'MyAPI/0.0.1'}
+                    subreddit_url = "https://reddit.com/r/{}/hot.json".format(subreddit)
+                        response = requests.get(subreddit_url, headers=headers)
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
-        print(None)
+                            if response.status_code == 200:
+                                        json_data = response.json()
+                                                posts = json_data.get('data', {}).get('children', [])
+                                                        if posts:
+                                                                        for i in range(min(10, len(posts))):
+                                                                                            print(posts[i].get('data', {}).get('title'))
+                                                                                                    else:
+                                                                                                                    print("OK")
+                                                                                                                        else:
+                                                                                                                                    print("OK")
