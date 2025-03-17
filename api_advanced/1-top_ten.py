8a3727401403cdff870a467aeec9e978d28778c6
Shina <m.atete@alustudent.com>
2025-03-17 17:44:05 +0200

    Changes

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,17 +8,14 @@ import requests
 
 
 def top_ten(subreddit):
-        """"top ten"""
-            url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
-                            .format(subreddit)
+    """"top ten"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
 
-                                res = requests.get(url,
-                                                                          headers={
-                                                                                                         'User-Agent': 'Mozilla/5.0'})
-
-                                                                              if res.status_code != 200:
-                                                                                          print("OK", end="")
-                                                                                              else:
-                                                                                                          json_response = res.json()
-                                                                                                                  posts = json_response.get('data').get('children')
-                                                                                                                          [print(post.get('data').get('title')) for post in posts]
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
+    
+    if res.status_code != 200:
+        print("OK", end="")                                                                  else:                                                                                      json_response = res.json()
+      posts = json_response.get('data').get('children')                                        [print(post.get('data').get('title')) for post in posts]
